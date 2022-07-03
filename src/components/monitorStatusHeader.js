ffe664d7dae12a68988e9816e8ae4b421fb7bb28
Steven <41929063+Embranchement@users.noreply.github.com>
2022-07-03 17:19:47 +0200

    Update monitorStatusHeader.js

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -23,9 +23,9 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         <div>{text}</div>
         {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
           <div className="text-xs font-light">
-            checked{' '}
+            vérifié{' '}
             {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
+            il y a (from{' '}
             {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
           </div>
         )}
